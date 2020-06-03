1bf6b2dd1d521daee97ef9833a93e2e3253583b4
ninadthepade <ninad.thepade@gmail.com>
2020-06-03 20:30:18 +0530

    storing all the values in indexedDB using localforage library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import {default as localforage} from 'localforage'
+
 import StockListContainer from './components/StockListContainer/StockListContainer'
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const ENDPOINT = 'ws://stocks.mnet.website';
+
+  useEffect(() => {
+    const ws = new WebSocket(ENDPOINT);
+
+    ws.onmessage = (event) => {
+      const response = JSON.parse(event.data);
+
+      let test = response.map(stock => {
+
+        let isNewPriceHigher;
+        // check if the item already exists in DB
+        localforage.getItem(stock[0]).then(value => {
+          // This code runs once the value has been loaded from the offline store.
+          isNewPriceHigher = (value.stockPrice < stock[1])
+
+          // set the item to the db
+          localforage.setItem(stock[0], {
+            'stockName': stock[0], 
+            'stockPrice': stock[1], 
+            'dateTime': Date.now(), 
+            'isNewPriceHigher': isNewPriceHigher 
+          }).then(value => console.log(value))
+          .catch(err => console.log(err))
+        }).catch(err => console.log(err))
+        return stock
+      })
+
+      setData(test);
+    };
+    ws.onclose = () => {
+      ws.close();
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [ENDPOINT]);
+
   return (
     <div className="App">
-      <StockListContainer />
+      <StockListContainer data={data} />
     </div>
   );
 }
--- src/components/StockListContainer/StockListContainer.js
+++ src/components/StockListContainer/StockListContainer.js
@@ -1,27 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import StockRow from '../StockRow/StockRow'
 
-const StockListContainer = () => {
-  const [data, setData] = useState([]);
-  const ENDPOINT = 'ws://stocks.mnet.website';
-
-  useEffect(() => {
-    const ws = new WebSocket(ENDPOINT);
-
-    ws.onmessage = (event) => {
-      const response = JSON.parse(event.data);
-      setData(response);
-    };
-    ws.onclose = () => {
-      ws.close();
-    };
-
-    return () => {
-      ws.close();
-    };
-  }, [ENDPOINT]);
-
-  console.log('data: ', data)
+const StockListContainer = ({data}) => {
   return (
     <div>
       {data.map((stock, index) => (
