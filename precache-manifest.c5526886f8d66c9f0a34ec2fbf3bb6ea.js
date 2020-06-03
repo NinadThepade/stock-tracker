ca99e0b825d63f1ae68b0ef5d4a84361c6ff26ca
ninadthepade <ninad.thepade@gmail.com>
2020-06-03 21:49:21 +0530

    Updates

---
+++ precache-manifest.c5526886f8d66c9f0a34ec2fbf3bb6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1f033ca02e4fc155a220e1bdbe5dc8",
+    "url": "/stock-tracker/index.html"
+  },
+  {
+    "revision": "178840d61e3b525f5d8f",
+    "url": "/stock-tracker/static/css/main.70fc6399.chunk.css"
+  },
+  {
+    "revision": "e97632dfbc4b5002807b",
+    "url": "/stock-tracker/static/js/2.3dcdeeb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-tracker/static/js/2.3dcdeeb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178840d61e3b525f5d8f",
+    "url": "/stock-tracker/static/js/main.4aecb4d6.chunk.js"
+  },
+  {
+    "revision": "296df7b74126eb412097",
+    "url": "/stock-tracker/static/js/runtime-main.7cc16266.js"
+  }
+]);
