7a9e29aee28064b2e57eba4e7b2728ed7f7d9d7f
ninadthepade <ninad.thepade@gmail.com>
2020-06-03 21:36:54 +0530

    Basic tracker working

--- src/components/StockRow/StockRow.js
+++ src/components/StockRow/StockRow.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './StockRow.css'
 
 const StockRow = ({stock}) => {
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  
+  useEffect(() => {
+    let dt = new Date(stock.dateTime).toLocaleString();
+    let dtArray = dt.split(',');
+    setDate(dtArray[0])
+    setTime(dtArray[1])
+  },[stock])
+
   return (
     <div className="stock-row">
       <div className="stock-name">{ stock.stockName }</div>
-      <div className="stock-price">{ stock.stockPrice }</div>
+      <div className={`stock-price ${stock.isNewPriceHigher ? "stock-price-higher" : "stock-price-lower"}`}>{ stock.stockPrice }</div>
+      <div className="stock-date">{ date }</div>
+      <div className="stock-time">{ time }</div>
     </div>
   )
 }
